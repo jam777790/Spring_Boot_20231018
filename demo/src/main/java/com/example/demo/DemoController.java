f67cb4b49f514daaa7bc30720098c5ad7a5c169c
Your name <you@example.com>
2026-09-15 21:13:49 +0900

    2주차 실습 완료

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -11,4 +11,15 @@ public class DemoController {
         model.addAttribute("data", " 반갑습니다."); // model 설정
         return "hello"; // hello.html 연결    
     }
+
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("data", " 홍길동님.");
+        model.addAttribute("data1", " 반갑습니다.");
+        model.addAttribute("data2", " 오늘.");
+        model.addAttribute("data3", " 날씨는.");
+        model.addAttribute("data4", " 매우 좋습니다."); // model 설정
+        return "hello2";
 }
+}
+
