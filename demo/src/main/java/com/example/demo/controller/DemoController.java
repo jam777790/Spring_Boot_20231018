2dea52b9543207371c853b8d072559cc8976d369
jam777790 <jam777790@sungkyul.ac.kr>
2026-09-23 13:59:36 +0900

    4주차 실습 완료

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,20 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
+
 @Controller
 public class DemoController {
+
+     @Autowired
+    TestService testService;
+
     @GetMapping("/hello") // 전송 방식 GET
     public String hello(Model model) {
         model.addAttribute("data", " 반갑습니다."); // model 설정
@@ -20,6 +29,16 @@ public class DemoController {
         model.addAttribute("data3", " 날씨는.");
         model.addAttribute("data4", " 매우 좋습니다."); // model 설정
         return "hello2";
-}
+    }
+
+    // DemoController 클래스 아래 객체 주입
+    // 하단에 맵핑 이어서 추가
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
+    }
 }
 
